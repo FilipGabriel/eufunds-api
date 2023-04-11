9914984ef85b394bf6ffe84f934a4ab6169850da
Gabriel <gabriel.finaru@smis.ro>
2023-04-11 12:02:49 +0300

    product option fix

--- Modules/Program/Http/Controllers/Api/ProgramProductController.php
+++ Modules/Program/Http/Controllers/Api/ProgramProductController.php
@@ -60,7 +60,7 @@ class ProgramProductController extends Controller
                     return [
                         'id' => $value->id,
                         'label' => $value->label . $value->formattedPriceForProduct($product),
-                        'amount' => $value->priceForProduct($product)->amount(),
+                        'amount' => (float) $value->priceForProduct($product)->amount(),
                     ];
                 })
             ];
