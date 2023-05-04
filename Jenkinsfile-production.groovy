281ce88dfff763cbba877fece65d6e9ff7789d0f
Gabriel <gabriel.finaru@smis.ro>
2023-05-04 13:23:35 +0300

    fix

--- Jenkinsfile-production.groovy
+++ Jenkinsfile-production.groovy
@@ -21,7 +21,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh("composer update")
+                sh("composer install")
                 sh("npm install lerna yarn")
                 sh("npm install --save-dev")
                 sh("npm run prod")
