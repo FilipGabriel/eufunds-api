95cde2bb3b06ba49bb73aeaf6194aa7af285630c
Gabriel <gabriel.finaru@smis.ro>
2023-05-16 08:38:45 +0300

    cookie fix

---
+++ Modules/Currency/Http/Controllers/Api/CurrentCurrencyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Currency\Http\Controllers\Api;
+
+class CurrentCurrencyController
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param string $currency
+     * @return \Illuminate\Http\Response
+     */
+    public function store($currency)
+    {
+        abort_if(! in_array($currency, setting('supported_currencies')), 403);
+
+        $cookie = cookie()->forever('currency', $currency);
+
+        return response()->json()->withCookie($cookie);
+    }
+}
--- Modules/Currency/Routes/api.php
+++ Modules/Currency/Routes/api.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('currency-rates', 'CurrencyRateController@index')->name('currency_rates.index');
+Route::get('current-currency/{code}', 'CurrentCurrencyController@store')->name('current_currency.store');
