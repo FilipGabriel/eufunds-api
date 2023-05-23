80cf9eebe59556b22aa41b7e9f0117238be93f9a
Gabriel <gabriel.finaru@smis.ro>
2023-05-23 11:25:49 +0300

    stock exceptions

--- Modules/Checkout/Resources/lang/ro/messages.php
+++ Modules/Checkout/Resources/lang/ro/messages.php
@@ -3,4 +3,6 @@
 return [
     'product_has_changed_price' => 'Produsul ":product" si-a modificat pretul.',
     'product_unavailable' => 'Produsul ":product" nu mai este valabil pentru acest program.',
+    'product_is_out_of_stock' => 'Produsul ":product" nu mai este in stoc.',
+    'product_doesn\'t_have_enough_stock' => 'Produsul ":product" nu are stoc suficient.',
 ];
