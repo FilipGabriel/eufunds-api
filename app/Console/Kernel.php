b014fb62c12c84a6bda09e7b72fb5fd9f97e015b
Gabriel <gabriel.finaru@smis.ro>
2023-08-25 18:26:23 +0300

    cronjobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,10 +30,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command("nod:import-categories")->daily()->timezone('Europe/Bucharest')->at('01:00');
-        // $schedule->command("nod:import-manufacturers")->daily()->timezone('Europe/Bucharest')->at('01:05');
-        // $schedule->command("nod:import-products")->daily()->timezone('Europe/Bucharest')->at('16:20');
-        // $schedule->command("nod:import-product-attributes")->daily()->timezone('Europe/Bucharest')->at('18:00');
+        $schedule->command("nod:import-categories")->daily()->timezone('Europe/Bucharest')->at('01:00');
+        $schedule->command("nod:import-manufacturers")->daily()->timezone('Europe/Bucharest')->at('01:15');
+        $schedule->command("nod:import-products")->daily()->timezone('Europe/Bucharest')->at('01:30');
+        $schedule->command("nod:import-product-attributes")->daily()->timezone('Europe/Bucharest')->at('03:00');
         $schedule->command("nod:import-product-info")->hourly()->timezone('Europe/Bucharest')->between('08:00', '00:00');
     }
 }
